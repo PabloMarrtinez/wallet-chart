db3fadd1e02ca3c2dec2540db2df402f2f306b51
PabloMarrtinez <pablomartinez9112001@gmail.com>
2024-05-24 12:06:59 +0200

    v1

---
+++ wallet/shared/script.sh
@@ -0,0 +1,3 @@
+cp -R /home/pablito/Desktop/git/umu-aries-framework-go-main/modules/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/* /home/pablito/Desktop/git/test/wallet/shared/cert.pem
+cp -R /home/pablito/Desktop/git/umu-aries-framework-go-main/modules/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* /home/pablito/Desktop/git/test/wallet/shared/key
+cp -R /home/pablito/Desktop/git/umu-aries-framework-go-main/modules/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt /home/pablito/Desktop/git/test/wallet/shared/ca.crt
